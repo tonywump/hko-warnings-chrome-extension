f89e6f3b987f1ced42a154869eef4cbceacbf51b
Tony Wu <mpwu@emsd.gov.hk>
2025-08-06 08:52:40 +0800

    update API

--- background.js
+++ background.js
@@ -1,9 +1,33 @@
+const warningSignal = {
+    "TC1":{"WarningName":"一號戒備信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/tc1.gif'},
+    "TC3":{"WarningName":"三號強風信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/tc3.gif'},
+    "TC8NE":{"WarningName":"八號東南烈風或暴風信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/tc8ne.gif'},
+    "TC8NW":{"WarningName":"八號西北烈風或暴風信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/tc8nw.gif'},
+    "TC8SE":{"WarningName":"八號東南烈風或暴風信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/tc8se.gif'},
+    "TC8SW":{"WarningName":"八號西南烈風或暴風信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/tc8sw.gif'},
+    "TC9":{"WarningName":"九號烈風或暴風風力增強信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/tc9.gif'},
+    "TC10":{"WarningName":"十號颶風信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/tc10.gif'},
+    "WRAINA":{"WarningName":"黃色暴雨警告信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/raina.gif'},
+    "WRAINR":{"WarningName":"紅色暴雨警告信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/rainr.gif'},
+    "WRAINB":{"WarningName":"黑色暴雨警告信號","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/rainb.gif'},
+    "WTS":{"WarningName":"雷暴警告","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/ts.gif'},
+    "WL":{"WarningName":"山泥傾瀉警告","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/landslip.gif'},
+    "WFROST":{"WarningName":"霜凍警告","Icon":'hhttps://www.hko.gov.hk/en/textonly/img/warn/images/frost.gif'},
+    "WFIREY":{"WarningName":"黃色火災危險警告","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/firey.gif'},
+    "WFIRER":{"WarningName":"紅色火災危險警告","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/firer.gif'},
+    "WCOLD":{"WarningName":"寒冷天氣警告","Icon":'https://www.hko.gov.hk/en/wservice/warning/images/warncold.gif'},
+    "WHOT":{"WarningName":"酷熱天氣警告","Icon":'https://www.hko.gov.hk/en/wservice/warning/images/warnhot.gif'},
+    "WTMW":{"WarningName":"海嘯警告","Icon":'https://www.hko.gov.hk/en/textonly/img/warn/images/tsunami-warn.gif'},
+    "WMSGNL":{"WarningName":"強烈季候風信號","Icon":'https://www.hko.gov.hk/en/wxinfo/dailywx/images/msn.gif'},
+}
+
+
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Extension installed");
 });
 
 function updateOverlay() {
-    return fetch('https://www.hko.gov.hk/wxinfo/dailywx/wxwarntoday_uc.json')
+    return fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warningInfo&lang=en')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok: ' + response.statusText);
@@ -13,13 +37,11 @@ function updateOverlay() {
         .then(data => {
             const warnings = [];
             const seenNames = new Set();
-            data['WARNING_DATABASE'].forEach(item => {
-                if (!seenNames.has(item.WarningName)) {
-                    seenNames.add(item.WarningName);
-                    warnings.push({
-                        WarningName: item.WarningName,
-                        Icon: 'https://www.hko.gov.hk' + item.Icon
-                    });
+            data['details'].forEach(item => {
+                const warningCode = item.subtype??item.warningStatementCode;
+                if (!seenNames.has(warningCode)) {
+                    seenNames.add(warningCode);
+                    warnings.push(warningSignal[warningCode]);
                 }
             });
             return warnings;
